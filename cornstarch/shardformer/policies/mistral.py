513dee209116c86dbdff928f3911f2cea1c79a94
Insu Jang <insujang@umich.edu>
2024-04-17 04:42:50 +0000

    Add pipeline template policy base implementation in mistral

--- cornstarch/shardformer/policies/mistral.py
+++ cornstarch/shardformer/policies/mistral.py
@@ -3,7 +3,9 @@ Code modified after copied from:
 https://github.com/hpcaitech/ColossalAI/blob/v0.3.6/colossalai/shardformer/policies/mistral.py
 """
 
+import itertools
 import warnings
+from typing import cast
 
 import torch.nn as nn
 from colossalai.shardformer.layer import (
@@ -18,14 +20,55 @@ from colossalai.shardformer.policies.base_policy import (
     Policy,
     SubModuleReplacementDescription,
 )
+from transformers import PretrainedConfig
+from transformers.models.mistral.configuration_mistral import MistralConfig
 
+from cornstarch.pipeline_template import PipelineTemplate
+from cornstarch.shardformer.policies.pipeline_template_policy import (
+    PipelineTemplatePolicyBase,
+)
 from cornstarch.shardformer.policies.utils import (
     resize_embeddings,
     resize_lm_head,
 )
 
 
-class MistralPolicy(Policy):
+class MistralPolicy(PipelineTemplatePolicyBase, Policy):
+    @staticmethod
+    def get_all_modules(config: PretrainedConfig) -> list[str]:
+        assert isinstance(
+            config, MistralConfig
+        ), "config must be an instance of MistralConfig"
+        config: MistralConfig = cast(MistralConfig, config)
+
+        modules = []
+        modules.append("embed_tokens")
+        modules.extend([f"layers.{i}" for i in range(config.num_hidden_layers)])
+        modules.append("norm")
+
+        return modules
+
+    def pipeline_template_sanity_check(self, template: PipelineTemplate):
+        assert (
+            "transformers.models.mistral.modeling_mistral" in template.model_name
+        ), "The pipeline template is not for the model that the policy is designed for."
+
+        prefix = "" if self.model.__class__.__name__ == "MistralModel" else "model."
+
+        assert hasattr(self.model, "config"), "model must have a config attribute"
+        modules = self.get_all_modules(self.model.config)
+        modules_in_template = list(itertools.chain(*template.modules_per_stage))
+        if modules != modules_in_template:
+            raise ValueError(
+                "Modules in the pipeline template do not match the modules in the model."
+            )
+
+        if f"{prefix}embed_tokens" not in modules_in_template[0]:
+            raise ValueError("embed_tokens must be in the first stage.")
+
+        if f"{prefix}norm" not in modules_in_template[-1]:
+            raise ValueError("norm must be in the last stage.")
+
     def config_sanity_check(self):
         pass
 
@@ -164,8 +207,12 @@ class MistralPolicy(Policy):
 
 
 class MistralModelPolicy(MistralPolicy):
-    def __init__(self) -> None:
-        super().__init__()
+    @staticmethod
+    def get_all_modules(config: PretrainedConfig) -> list[str]:
+        return MistralPolicy.get_all_modules(config)
+
+    def pipeline_template_sanity_check(self, template: PipelineTemplate):
+        super().pipeline_template_sanity_check(template)
 
     def module_policy(self):
         if self.pipeline_stage_manager:
@@ -175,6 +222,19 @@ class MistralModelPolicy(MistralPolicy):
 
 
 class MistralForCausalLMPolicy(MistralPolicy):
+    @staticmethod
+    def get_all_modules(config: PretrainedConfig) -> list[str]:
+        modules = [
+            f"model.{module}" for module in MistralPolicy.get_all_modules(config)
+        ]
+        modules.append("lm_head")
+        return modules
+
+    def pipeline_template_sanity_check(self, template: PipelineTemplate):
+        super().pipeline_template_sanity_check(template)
+        if "lm_head" not in template.modules_per_stage[-1]:
+            raise ValueError("lm_head must be in the last stage.")
+
     def module_policy(self):
         from transformers import MistralForCausalLM
 
@@ -203,6 +263,19 @@ class MistralForCausalLMPolicy(MistralPolicy):
 
 
 class MistralForSequenceClassificationPolicy(MistralPolicy):
+    @staticmethod
+    def get_all_modules(config: PretrainedConfig) -> list[str]:
+        modules = [
+            f"model.{module}" for module in MistralPolicy.get_all_modules(config)
+        ]
+        modules.append("score")
+        return modules
+
+    def pipeline_template_sanity_check(self, template: PipelineTemplate):
+        super().pipeline_template_sanity_check(template)
+        if "score" not in template.modules_per_stage[-1]:
+            raise ValueError("score must be in the last stage.")
+
     def module_policy(self):
         from transformers import MistralForSequenceClassification
 
