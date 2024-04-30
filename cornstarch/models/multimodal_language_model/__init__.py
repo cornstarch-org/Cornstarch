3b8157d19fe01ae929b9e9303778da61611646b9
Insu Jang <insujang@umich.edu>
2024-04-30 04:35:10 +0000

    Register projector to automodel

--- cornstarch/models/multimodal_language_model/__init__.py
+++ cornstarch/models/multimodal_language_model/__init__.py
@@ -1,3 +1,13 @@
-from .configuration_multimodal_language_model import MultimodalLanguageModelConfig
-from .modeling_multimodal_language_model import MultimodalLanguageModel
+from transformers import AutoModel
+
+from .configuration_multimodal_language_model import (
+    MultimodalLanguageModelConfig,
+    MultimodalLanguageModelProjectorConfig,
+)
+from .modeling_multimodal_language_model import (
+    MultimodalLanguageModel,
+    MultimodalProjectorModel,
+)
 from .processing_multimodal_language_model import MultimodalLanguageModelProcessor
+
+AutoModel.register(MultimodalLanguageModelProjectorConfig, MultimodalProjectorModel)
