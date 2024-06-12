f68d1271966abe3e03c2e8c9830f0175b61c0f13
Insu Jang <insujang@umich.edu>
2024-06-12 04:42:40 +0000

    Refacotr multimodal model

--- cornstarch/models/multimodal_language_model/__init__.py
+++ cornstarch/models/multimodal_language_model/__init__.py
@@ -1,27 +1,13 @@
 from transformers import AutoConfig, AutoModel
 
-from cornstarch.models.evaclip import (
-    EvaCLIPConfig,
-    EvaCLIPPreTrainedModel,
-    EvaCLIPVisionConfig,
-    EvaCLIPVisionModel,
-)
-from cornstarch.models.intern_vit import InternVisionConfig, InternVisionModel
-
-from .configuration_multimodal_language_model import (
-    MultimodalLanguageModelConfig,
-    MultimodalLanguageModelProjectorConfig,
-)
+from .configuration_multimodal_language_model import ProjectorModelConfig
 from .modeling_multimodal_language_model import (
-    MultimodalLanguageModel,
-    MultimodalProjectorModel,
+    ModalModule,
+    ModalType,
+    MultimodalModel,
+    ProjectorModel,
 )
 from .processing_multimodal_language_model import MultimodalLanguageModelProcessor
 
-AutoModel.register(MultimodalLanguageModelProjectorConfig, MultimodalProjectorModel)
-
-AutoModel.register(EvaCLIPConfig, EvaCLIPPreTrainedModel)
-AutoModel.register(EvaCLIPVisionConfig, EvaCLIPVisionModel)
-
-AutoConfig.register("intern_vit_6b", InternVisionConfig)
-AutoModel.register(InternVisionConfig, InternVisionModel)
+AutoConfig.register("projector-model", ProjectorModelConfig)
+AutoModel.register(ProjectorModelConfig, ProjectorModel)
