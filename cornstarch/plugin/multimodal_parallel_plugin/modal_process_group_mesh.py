066bb6295c6a56bf6e671f17915f0f9d70f50e76
Insu Jang <insujang@umich.edu>
2024-06-20 22:02:25 +0000

    Update multimodal group mesh Now one group mesh includes all ranks for every modals.

--- cornstarch/plugin/multimodal_parallel_plugin/modal_process_group_mesh.py
+++ cornstarch/plugin/multimodal_parallel_plugin/modal_process_group_mesh.py
@@ -1,43 +1,200 @@
-from typing import List
+import itertools
+from collections import defaultdict, deque
 
+import numpy as np
 import torch.distributed as dist
-from colossalai.cluster.process_group_mesh import ProcessGroupMesh, prod
+from colossalai.cluster.process_group_mesh import ProcessGroupMesh
+
+from cornstarch.pipeline_template import PipelineTemplate
 
 
-class ModalProcessGroupMesh(ProcessGroupMesh):
+class MultiModalProcessGroupMesh(ProcessGroupMesh):
     """
-    Different from the original `ProgressGroupMesh`, which includes all ranks,
-    `ModalProcessGroupMesh` only includes ranks for a specific modal.
-    This makes several differences in implementation.
-    TODO: fix it!
+    Different from the original `ProcessGroupMesh`, which assumes a unimodal model is given,
+    `MultimodalProcessGroupMesh` manages graph of models.
+
+    This makes several differences in implementation:
+    - First stage rank may be in the middle of the rank array.
     """
 
-    def __init__(self, group: dist.ProcessGroup, *size: int) -> None:
+    def __init__(
+        self,
+        modal_templates: dict[PipelineTemplate, int],
+        execution_order: list[tuple[PipelineTemplate, PipelineTemplate]],
+    ) -> None:
         assert dist.is_initialized(), "Please initialize torch.distributed first."
-        world_size = dist.get_world_size(group)
-        prod_size = prod(size)
-        if world_size != -1:
+        assert len(modal_templates) > 0, "At least one modal is required."
+        for from_modal, to_modal in execution_order:
             assert (
-                prod_size == world_size
-            ), f"The product of the size({prod_size}) must be equal to the group world size({world_size})."
-            self._shape = size
-            self._rank = dist.get_rank(group)
-            self._coord = ProcessGroupMesh.unravel(self._rank, self._shape)
+                from_modal in modal_templates
+            ), f"{from_modal} not in modal_templates."
+            assert to_modal in modal_templates, f"{to_modal} not in modal_templates."
 
+        topological_sorted_modals = self.topological_sort(execution_order)
+        assert len(modal_templates) == len(topological_sorted_modals)
+
+        assert (
+            dist.get_world_size()
+            % sum(
+                template.num_stages * tp_size
+                for template, tp_size in modal_templates.items()
+            )
+            == 0
+        ), "The world size must be divisible by tp_size * pp_size."
+        dp_size = dist.get_world_size() // sum(
+            template.num_stages * tp_size
+            for template, tp_size in modal_templates.items()
+        )
+
+        mesh = []
+        max_tp_size = max(modal_templates.values())
+        rank_index = 0
+        for _ in range(dp_size):
+            dp_mesh = []
+            for modal in topological_sorted_modals:
+                tp_size = modal_templates[modal]
+                for _ in range(modal.num_stages):
+                    # create a list of ranks with length `max_tp_size`, where each rank is repeated `max_tp_size // tp_size` times.
+                    # Example: [0, 0, 1, 1] for tp_size=2 and max_tp_size=4
+                    ranks = [
+                        i
+                        for i in range(rank_index, rank_index + tp_size)
+                        for _ in range(max_tp_size // tp_size)
+                    ]
+                    dp_mesh.append(ranks)
+                    rank_index += tp_size
+            mesh.append(dp_mesh)
+        self._mesh = np.array(mesh)
+        self._shape = self._mesh.shape
+
+        self._rank = dist.get_rank()
+        self._coords = MultiModalProcessGroupMesh.unravel(self._rank, self._mesh)
         self._ranks_to_group: dict[tuple[int, ...], dist.ProcessGroup] = {}
         self._group_to_ranks: dict[dist.ProcessGroup, tuple[int, ...]] = {}
-        self._group = group
 
-    def get_group(
-        self, ranks_in_group: List[int], backend: str | None = None
+    @property
+    def coords(self) -> list[tuple[int, ...]]:
+        """The process coordinates.
+
+        Returns:
+            list[tuple[int, ...]]: The process coordinates.
+        """
+        return self._coords
+
+    @property
+    def mesh(self) -> np.ndarray:
+        """The process rank mesh.
+
+        Returns:
+            np.ndarray: The process rank mesh.
+        """
+        return self._mesh
+
+    @staticmethod
+    def unravel(rank: int, mesh: np.ndarray) -> list[tuple[int, ...]]:
+        """Convert a rank to a list of coordinates.
+
+        Unlike colossalai.cluster.process_group_mesh.ProcessGroupMesh.unravel,
+        our mesh manages process groups per layer; hence the same rank can exist
+        in multiple coordinates.
+
+        Args:
+            rank (int): Rank to be converted.
+            mesh (tuple[int, ...]): A grid of process ranks.
+
+        Returns:
+            list[tuple[int, ...]]: List of coordinates of the rank.
+        """
+        indices = np.where(mesh == rank)
+        return list(zip(*indices))
+
+    def topological_sort(
+        self, execution_order: list[tuple[PipelineTemplate, PipelineTemplate]]
+    ) -> list[PipelineTemplate]:
+        """
+        Topological sort the modal templates based on the execution order.
+        """
+        graph: dict[PipelineTemplate, list[PipelineTemplate]] = defaultdict(list)
+        in_degree: dict[PipelineTemplate, int] = defaultdict(int)
+
+        for from_modal, to_modal in execution_order:
+            graph[from_modal].append(to_modal)
+            in_degree[to_modal] += 1
+            if from_modal not in in_degree:
+                in_degree[from_modal] = 0
+
+        # Find all modals with no incoming edges
+        zero_in_degree = deque([modal for modal in in_degree if in_degree[modal] == 0])
+        topological_order: list[PipelineTemplate] = []
+
+        while zero_in_degree:
+            node = zero_in_degree.popleft()
+            topological_order.append(node)
+
+            # Reduce the in-degre of each neigher by 1
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    zero_in_degree.append(neighbor)
+
+        # Check if topological sort is possible
+        if len(topological_order) == len(in_degree):
+            return topological_order
+        else:
+            raise ValueError("Topological sort is not possible.")
+
+    def create_group_along_axis(
+        self,
+        axis: int,
+        indices_at_axis: list[int],
+        backend: str | None = None,
     ) -> dist.ProcessGroup:
-        ranks_in_group = sorted(ranks_in_group)
-        if tuple(ranks_in_group) not in self._group_to_ranks:
-            # Convert local ranks to global ranks
-            global_ranks_in_group = [
-                dist.get_global_rank(self._group, rank) for rank in ranks_in_group
-            ]
-            group = dist.new_group(global_ranks_in_group, backend=backend)
-            self._ranks_to_group[tuple(ranks_in_group)] = group
-            self._group_to_ranks[group] = tuple(ranks_in_group)
-        return super().get_group(ranks_in_group, backend)
+        indices_at_axis = indices_at_axis or list(range(self._shape[axis]))
+        reduced_shape = list(self._shape)
+        # the choices on the axis are reduced to 1, since it's determined by `indices_at_axis`
+        reduced_shape[axis] = 1
+        target_group = None
+        # use Cartesian product to generate all combinations of coordinates
+        for base_coord in itertools.product(*[range(s) for s in reduced_shape]):
+            coords_in_group = ProcessGroupMesh.get_coords_along_axis(
+                base_coord, axis, indices_at_axis
+            )
+            ranks_in_group = tuple(
+                set([self._mesh[coord] for coord in coords_in_group])
+            )
+
+            group = self.get_group(ranks_in_group, backend=backend)
+            if self._rank in ranks_in_group:
+                target_group = group
+        return target_group
+
+    def get_group_along_axis(
+        self,
+        axis: int,
+        indices_at_axis: list[int] | None = None,
+        backend: str | None = None,
+    ) -> dist.ProcessGroup:
+        """Get the process group along the given axis which the current process belongs to.
+        If the process group doesn't exist, it will be created.
+
+        A rank may exist multiple times in the mesh, but it should belong to the same tp group.
+        If axis is DP, it may belong to different dp groups; however, all dp groups must have
+        the same set of ranks, thus it should return only one process group.
+
+        Args:
+            axis (int): The axis along which the group is created.
+            indices_at_axis (list[int], optional): The indices at the axis. Defaults to None.
+            backend (str, optional): The backend to create the group. Defaults to None.
+
+        Returns:
+            ProcessGroup: The process group along the given axis which the current process belongs to.
+        """
+        indices_at_axis = indices_at_axis or list(range(self._shape[axis]))
+
+        coords_in_group = self.get_coords_along_axis(
+            self._coords[0], axis, indices_at_axis
+        )
+        ranks_in_group = tuple(set([self._mesh[coord] for coord in coords_in_group]))
+        if ranks_in_group not in self._ranks_to_group:
+            return self.create_group_along_axis(axis, indices_at_axis, backend=backend)
+        return self._ranks_to_group[ranks_in_group]
--- tests/test_plugins/multimodal_parallel_plugin/test_modal_process_group_mesh.py
+++ tests/test_plugins/multimodal_parallel_plugin/test_modal_process_group_mesh.py
@@ -6,40 +6,115 @@ import torch.distributed as dist
 from pytest_mock import MockerFixture
 from torch.testing._internal.distributed.fake_pg import FakeStore
 
+from cornstarch.pipeline_template import PipelineTemplate
 from cornstarch.plugin.multimodal_parallel_plugin.modal_process_group_mesh import (
-    ModalProcessGroupMesh,
+    MultiModalProcessGroupMesh,
+)
+
+encoder1_template = PipelineTemplate(
+    "encoder1", [["layer.0", "layer.1"], ["layer.2", "layer.3"]]
+)
+encoder2_template = PipelineTemplate(
+    "encoder2", [["layer.0", "layer.1"], ["layer.2", "layer.3"], ["layer.4", "layer.5"]]
+)
+
+llm_template_2stages = PipelineTemplate(
+    "llm", [["layer.0", "layer.1", "layer.2"], ["layer.3", "layer.4", "layer.5"]]
+)
+llm_template_4stages = PipelineTemplate(
+    "llm",
+    [
+        ["layer.0", "layer.1"],
+        ["layer.2", "layer.3"],
+        ["layer.4", "layer.5"],
+        ["layer.6", "layer.7"],
+    ],
 )
 
 
 @pytest.mark.parametrize(
-    "ranks_per_modal",
+    "world_size, modal_template, execution_order, expected_mesh",
     [
-        [
-            ([0, 1, 2, 3, 12, 13, 14, 15], (2, 2)),
-            ([4, 5, 6, 7, 8, 9, 10, 11, 16, 17, 18, 19, 20, 21, 22, 23], (2, 4)),
-        ],
-        [
-            ([0, 1, 2, 3, 8, 9, 10, 11, 16, 17, 18, 19], (2, 2)),
-            (
-                [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23],
-                (2, 2),
-            ),
-        ],
-        [
-            (
-                [0, 1, 2, 3, 12, 13, 14, 15],
-                (2, 2),
-            ),
-            (
-                [4, 5, 6, 7, 16, 17, 18, 19],
-                (2, 2),
-            ),
-            ([8, 9, 10, 11, 20, 21, 22, 23], (2, 2)),
-        ],
+        (
+            24,
+            {encoder1_template: 2, llm_template_2stages: 4},
+            [(encoder1_template, llm_template_2stages)],
+            [
+                [
+                    [0, 0, 1, 1],  # encoder1, stage1
+                    [2, 2, 3, 3],  # encoder1, stage2
+                    [4, 5, 6, 7],  # llm, stage1
+                    [8, 9, 10, 11],  # llm, stage2
+                ],
+                [
+                    [12, 12, 13, 13],
+                    [14, 14, 15, 15],
+                    [16, 17, 18, 19],
+                    [20, 21, 22, 23],
+                ],
+            ],
+        ),
+        (
+            18,
+            {encoder1_template: 2, encoder2_template: 2, llm_template_2stages: 4},
+            [
+                (encoder1_template, llm_template_2stages),
+                (encoder2_template, llm_template_2stages),
+            ],
+            [
+                [
+                    [0, 0, 1, 1],  # encoder1, stage1
+                    [2, 2, 3, 3],  # encoder1, stage2
+                    [4, 4, 5, 5],  # encoder2, stage1
+                    [6, 6, 7, 7],  # encoder2, stage2
+                    [8, 8, 9, 9],  # encoder2, stage3
+                    [10, 11, 12, 13],  # llm, stage1
+                    [14, 15, 16, 17],  # llm, stage2
+                ],
+            ],
+        ),
+        (
+            84,
+            {encoder2_template: 4, llm_template_4stages: 4},
+            [(encoder2_template, llm_template_4stages)],
+            [
+                [
+                    [0, 1, 2, 3],  # encoder2, stage1
+                    [4, 5, 6, 7],  # encoder2, stage2
+                    [8, 9, 10, 11],  # encoder2, stage3
+                    [12, 13, 14, 15],  # llm, stage1
+                    [16, 17, 18, 19],  # llm, stage2
+                    [20, 21, 22, 23],  # llm, stage3
+                    [24, 25, 26, 27],  # llm, stage4
+                ],
+                [
+                    [28, 29, 30, 31],
+                    [32, 33, 34, 35],
+                    [36, 37, 38, 39],
+                    [40, 41, 42, 43],
+                    [44, 45, 46, 47],
+                    [48, 49, 50, 51],
+                    [52, 53, 54, 55],
+                ],
+                [
+                    [56, 57, 58, 59],
+                    [60, 61, 62, 63],
+                    [64, 65, 66, 67],
+                    [68, 69, 70, 71],
+                    [72, 73, 74, 75],
+                    [76, 77, 78, 79],
+                    [80, 81, 82, 83],
+                ],
+            ],
+        ),
     ],
 )
 def test_get_group(
-    ranks_per_modal: list[tuple[list[int], tuple[int, int]]], mocker: MockerFixture
+    world_size: int,
+    modal_template: dict[PipelineTemplate, int],
+    execution_order: list[tuple[PipelineTemplate, PipelineTemplate]],
+    expected_mesh: list[list[list[int]]],
+    mocker: MockerFixture,
 ):
     recorded_new_group_calls: dict[int, list] = defaultdict(list)
 
@@ -57,23 +132,18 @@ def test_get_group(
         wraps=record_new_group_call_decorator(dist.new_group),
     )
 
-    for rank in range(24):
+    meshes = []
+    for rank in range(world_size):
         dist.init_process_group(
-            backend="fake", store=FakeStore(), rank=rank, world_size=24
+            backend="fake", store=FakeStore(), rank=rank, world_size=world_size
         )
 
-        meshes = []
-
-        for ranks_for_modal, size in ranks_per_modal:
-            group = dist.new_group(ranks_for_modal)
-            tp_size, pp_size = size
-            dp_size = len(ranks_for_modal) // (tp_size * pp_size)
-            mesh = ModalProcessGroupMesh(group, pp_size, dp_size, tp_size)
-            meshes.append(mesh)
+        mesh = MultiModalProcessGroupMesh(modal_template, execution_order)
+        assert (mesh.mesh == expected_mesh).all()
+        meshes.append(mesh)
+        mesh.get_group_along_axis(0)
 
         dist.destroy_process_group()
 
-    for rank, records in recorded_new_group_calls.items():
-        # All ranks must call new_group in the same order with the same list of ranks.
-        assert len(records) == len(ranks_per_modal)
-        assert records == [ranks[0] for ranks in ranks_per_modal]
+    for rank, group_calls in recorded_new_group_calls.items():
+        assert group_calls == record_new_group_call_decorator[0]
