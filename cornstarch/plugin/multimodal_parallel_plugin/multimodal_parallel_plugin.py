c1a85169bd2070c67534e5e160ef70180a291509
Insu Jang <insujang@umich.edu>
2024-06-27 21:32:47 +0000

    add arguments to plugin init

--- cornstarch/plugin/multimodal_parallel_plugin/multimodal_parallel_plugin.py
+++ cornstarch/plugin/multimodal_parallel_plugin/multimodal_parallel_plugin.py
@@ -1,8 +1,10 @@
 from typing import Any, Callable, Tuple
 
+import torch.distributed as dist
 from colossalai.booster.plugin.hybrid_parallel_plugin import HybridParallelPlugin
 from colossalai.booster.plugin.pp_plugin_base import PipelinePluginBase
 from colossalai.interface import OptimizerWrapper
+from colossalai.shardformer import ShardConfig
 from torch.nn import Module
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler as LRScheduler
@@ -13,15 +15,69 @@ from cornstarch.plugin.multimodal_parallel_plugin import ModalParallelPlugin
 
 
 class MultimodalParallelPlugin(HybridParallelPlugin):
+    """Plugin for multimodal language model.
+    Tensor parallel, pipeline parallel, and data parallel are combined in this plugin.
+    Each modal has its own parallel configuration defined in ModalParallelPlugin.
+    """
+
     def __init__(
         self,
-        modal_plugins: dict[str, ModalParallelPlugin],
+        encoder_plugins: dict[str, ModalParallelPlugin] = None,
+        language_model_plugin: ModalParallelPlugin | None = None,
+        precision: str = "fp16",
+        enable_fused_normalization: bool = False,
+        enable_flash_attention: bool = False,
+        enable_jit_fused: bool = False,
+        num_microbatches: int = None,
+        microbatch_size: int = None,
+        initial_scale: float = 2**16,
+        min_scale: float = 1,
+        growth_factor: float = 2,
+        backoff_factor: float = 0.5,
+        growth_interval: int = 1000,
+        hysteresis: int = 2,
+        max_scale: float = 2**32,
+        max_norm: float = 0,
+        parallel_output: bool = True,
+        make_vocab_size_divisible_by: int = 64,
     ):
         PipelinePluginBase.__init__(self)
-        self.modal_plugins = modal_plugins
+        self.encoder_plugins = encoder_plugins
+        self.language_model_plugin = language_model_plugin
 
-    def add_modal_plugin(self, modal_name: str, plugin: ModalParallelPlugin):
-        self.modal_plugins[modal_name] = plugin
+        self.precision = precision
+        self.zero_stage = 0
+        self.microbatch_size = microbatch_size
+        self.num_microbatch = self.num_microbatch
+        self.global_batch_size = microbatch_size * num_microbatches
+        self.max_norm = max_norm
+
+        self.shard_config = ShardConfig(
+            tensor_parallel_process_group=None,
+            enable_tensor_parallelism=False,
+            pipeline_stage_manager=None,
+            enable_all_optimization=False,
+            enable_fused_normalization=enable_fused_normalization,
+            enable_flash_attention=enable_flash_attention,
+            enable_jit_fused=enable_jit_fused,
+            enable_sequence_parallelism=False,
+            enable_sequence_overlap=False,
+            parallel_output=parallel_output,
+            make_vocab_size_divisible_by=make_vocab_size_divisible_by,
+        )
+
+        self.amp_config = dict(
+            initial_scale=initial_scale,
+            growth_factor=growth_factor,
+            backoff_factor=backoff_factor,
+            growth_interval=growth_interval,
+            hysteresis=hysteresis,
+            min_scale=min_scale,
+            max_scale=max_scale,
+        )
+
+    def add_encoder_plugins(self, name: str, plugin: ModalParallelPlugin):
+        self.encoder_plugins[name] = plugin
 
     @property
     def enable_pipeline_parallelism(self) -> bool:
@@ -43,6 +99,7 @@ class MultimodalParallelPlugin(HybridParallelPlugin):
         return True
 
     def support_lora(self) -> bool:
+        """LoRA must manually be added to each modal before generating the plugin."""
         return False
 
     def control_checkpoint_io(self) -> bool:
