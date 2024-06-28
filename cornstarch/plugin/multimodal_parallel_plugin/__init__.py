8a4cb30c992a155d0da487403cdce92d668b39ea
Insu Jang <insujang@umich.edu>
2024-06-28 04:11:10 +0000

    Add classes into init

--- cornstarch/plugin/multimodal_parallel_plugin/__init__.py
+++ cornstarch/plugin/multimodal_parallel_plugin/__init__.py
@@ -1,3 +1,5 @@
 from .modal_parallel_plugin import ModalParallelPlugin
+from .modal_process_group_mesh import MultiModalProcessGroupMesh
 from .multimodal_parallel_checkpoint_io import MultimodalParallelCheckpointIO
 from .multimodal_parallel_plugin import MultimodalParallelPlugin
+from .multimodal_stage_manager import MultiModalPipelineStageManager
