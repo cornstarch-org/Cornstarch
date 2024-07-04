4ffea632ab71b83b7422fa5f9346db715c204cf6
Insu Jang <insujang@umich.edu>
2024-07-04 18:14:16 +0000

    add mistral policy with cache_position

--- cornstarch/shardformer/policies/mistral.py
+++ cornstarch/shardformer/policies/mistral.py
@@ -1,17 +1,38 @@
+import functools
 import itertools
-from typing import cast
+import warnings
+from typing import Callable, Dict, List, cast
 
-from colossalai.shardformer.policies.base_policy import Policy
+from colossalai.shardformer.layer import (
+    FusedRMSNorm,
+    Linear1D_Col,
+    Linear1D_Row,
+    PaddingEmbedding,
+    PaddingLMHead,
+    VocabParallelEmbedding1D,
+    VocabParallelLMHead1D,
+)
+from colossalai.shardformer.policies.base_policy import (
+    ModulePolicyDescription,
+    Policy,
+    SubModuleReplacementDescription,
+)
 from colossalai.shardformer.policies.mistral import (
     MistralForCausalLMPolicy as ColossalMistralForCausalLMPolicy,
 )
 from colossalai.shardformer.policies.mistral import (
     MistralModelPolicy as ColossalMistralModelPolicy,
 )
+from torch import nn
 from transformers import PretrainedConfig
 from transformers.models.mistral.configuration_mistral import MistralConfig
+from transformers.models.mistral.modeling_mistral import MistralModel
 
 from cornstarch.pipeline_template import PipelineTemplate
+from cornstarch.shardformer.modeling.mistral import (
+    MistralForwards,
+    MistralPipelineForwards,
+)
 from cornstarch.shardformer.policies.pipeline_template_policy import (
     PipelineTemplatePolicyBase,
 )
@@ -53,14 +74,215 @@ class MistralPolicy(PipelineTemplatePolicyBase, Policy):
         if f"{prefix}norm" not in modules_in_template[-1]:
             raise ValueError("norm must be in the last stage.")
 
+    def set_pipeline_forward(
+        self, model_cls: nn.Module, new_forward: Callable, policy: dict
+    ):
+        if self.pipeline_stage_manager is None:
+            return
+
+        stage_manager = self.pipeline_stage_manager
+        if self.model.__class__.__name__ == "MistralModel":
+            module = self.model
+        else:
+            module = self.model.model
+
+        layers_per_stage = stage_manager.distribute_layers(
+            len(module.layers), stage_manager.num_stages
+        )
+        stage_index = stage_manager.get_stage_index(
+            layers_per_stage, stage_manager.stage
+        )
+        method_replacement = {
+            "forward": functools.partial(
+                new_forward,
+                stage_manager=stage_manager,
+                stage_index=stage_index,
+                shard_config=self.shard_config,
+            )
+        }
+        self.append_or_create_method_replacement(
+            description=method_replacement, policy=policy, target_key=model_cls
+        )
+
+    def get_held_layers(self) -> List[nn.Module]:
+        assert self.pipeline_stage_manager is not None
+
+        module: MistralModel
+        if self.model.__class__.__name__ == "MistralModel":
+            module = self.model
+        else:
+            module = self.model.model
+        stage_manager = self.pipeline_stage_manager
+
+        held_layers = []
+        if stage_manager.is_interleave:
+            assert stage_manager.num_model_chunks is not None
+            layers_per_stage = stage_manager.distribute_layers(len(module.layers))
+            stage_indices = stage_manager.get_stage_index(layers_per_stage)
+            if stage_manager.is_first_stage(ignore_chunk=True):
+                held_layers.append(module.embed_tokens)
+            for start_idx, end_idx in stage_indices:
+                held_layers.extend(module.layers[start_idx:end_idx])
+            if stage_manager.is_last_stage(ignore_chunk=True):
+                held_layers.append(module.norm)
+
+        else:
+            layers_per_stage = stage_manager.distribute_layers(len(module.layers))
+            if stage_manager.is_first_stage():
+                held_layers.append(module.embed_tokens)
+            start_idx, end_idx = stage_manager.get_stage_index(layers_per_stage)
+            held_layers.extend(module.layers[start_idx:end_idx])
+            if stage_manager.is_last_stage():
+                held_layers.append(module.norm)
+        return held_layers
+
+    def module_policy(self) -> Dict[str | nn.Module, ModulePolicyDescription]:
+        from transformers.models.mistral.modeling_mistral import (
+            MistralAttention,
+            MistralDecoderLayer,
+            MistralFlashAttention2,
+            MistralSdpaAttention,
+        )
+
+        policy = {}
+
+        if self.shard_config.enable_sequence_parallelism:
+            self.shard_config.enable_sequence_parallelism = False
+            warnings.warn(
+                "Mistral doesn't support sequence parallelism now, will ignore the sequence parallelism flag."
+            )
+
+        if self.shard_config.enable_tensor_parallelism:
+            assert (
+                self.model.config.num_attention_heads
+                % self.shard_config.tensor_parallel_size
+                == 0
+            ), "The number of attention heads must be divisible by tensor parallel size."
+            assert (
+                self.model.config.num_key_value_heads
+                % self.shard_config.tensor_parallel_size
+                == 0
+            ), "The number of key_value heads must be divisible by tensor parallel size."
+
+            decoder_attribute_replacement = {
+                "self_attn.hidden_size": self.model.config.hidden_size
+                // self.shard_config.tensor_parallel_size,
+                "self_attn.num_heads": self.model.config.num_attention_heads
+                // self.shard_config.tensor_parallel_size,
+                "self_attn.num_key_value_heads": self.model.config.num_key_value_heads
+                // self.shard_config.tensor_parallel_size,
+            }
+
+            policy[MistralDecoderLayer] = ModulePolicyDescription(
+                attribute_replacement=decoder_attribute_replacement,
+                sub_module_replacement=[
+                    SubModuleReplacementDescription(
+                        suffix="self_attn.q_proj",
+                        target_module=Linear1D_Col,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="self_attn.k_proj",
+                        target_module=Linear1D_Col,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="self_attn.v_proj",
+                        target_module=Linear1D_Col,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="self_attn.o_proj",
+                        target_module=Linear1D_Row,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="mlp.gate_proj",
+                        target_module=Linear1D_Col,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="mlp.up_proj",
+                        target_module=Linear1D_Col,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="mlp.down_proj",
+                        target_module=Linear1D_Row,
+                    ),
+                ],
+            )
+
+        embedding_cls = None
+        if self.shard_config.enable_tensor_parallelism:
+            embedding_cls = VocabParallelEmbedding1D
+        elif self.tie_weight:
+            embedding_cls = PaddingEmbedding
+
+        if embedding_cls is not None:
+            self.append_or_create_submodule_replacement(
+                description=SubModuleReplacementDescription(
+                    suffix="embed_tokens",
+                    target_module=embedding_cls,
+                    kwargs={
+                        "make_vocab_size_divisible_by": self.shard_config.make_vocab_size_divisible_by
+                    },
+                ),
+                policy=policy,
+                target_key=MistralModel,
+            )
+
+        # optimization configuration
+        if self.shard_config.enable_fused_normalization:
+            self.append_or_create_submodule_replacement(
+                description=[
+                    SubModuleReplacementDescription(
+                        suffix="input_layernorm",
+                        target_module=FusedRMSNorm,
+                    ),
+                    SubModuleReplacementDescription(
+                        suffix="post_attention_layernorm",
+                        target_module=FusedRMSNorm,
+                    ),
+                ],
+                policy=policy,
+                target_key=MistralDecoderLayer,
+            )
+
+            self.append_or_create_submodule_replacement(
+                description=SubModuleReplacementDescription(
+                    suffix="norm",
+                    target_module=FusedRMSNorm,
+                ),
+                policy=policy,
+                target_key=MistralModel,
+            )
+
+        if self.shard_config.enable_flash_attention:
+            ATTN_IMPLEMENTATION = {
+                "eager": MistralAttention,
+                "flash_attention_2": MistralFlashAttention2,
+                "sdpa": MistralSdpaAttention,
+            }
+            attn_cls = ATTN_IMPLEMENTATION[self.model.config._attn_implementation]
+            self.append_or_create_method_replacement(
+                description={
+                    "forward": MistralForwards.mistral_flash_attention_forward,
+                },
+                policy=policy,
+                target_key=attn_cls,
+            )
+            if self.pipeline_stage_manager is None:
+                # replace llama model forward method
+                self.append_or_create_method_replacement(
+                    description={
+                        "forward": functools.partial(
+                            MistralForwards.mistral_model_forward_for_flash_attention,
+                            shard_config=self.shard_config,
+                        ),
+                    },
+                    policy=policy,
+                    target_key=MistralModel,
+                )
+
+        return policy
+
 
 class MistralModelPolicy(MistralPolicy, ColossalMistralModelPolicy):
-    def preprocess(self):
-        # This is for forward compatibility to support newer version of transformers
-        self.model._use_flash_attention_2 = self.shard_config.enable_flash_attention
-        self.tie_weight = self.tie_weight_check()
-        return self.model
-
     @staticmethod
     def get_all_modules(config: PretrainedConfig) -> list[str]:
         return MistralPolicy.get_all_modules(config)
@@ -68,16 +290,23 @@ class MistralModelPolicy(MistralPolicy, ColossalMistralModelPolicy):
     def pipeline_template_sanity_check(self, template: PipelineTemplate):
         super().pipeline_template_sanity_check(template)
 
+    def module_policy(self) -> Dict[str | nn.Module, ModulePolicyDescription]:
+        policy = super().module_policy()
+
+        if self.pipeline_stage_manager:
+            self.set_pipeline_forward(
+                model_cls=MistralModel,
+                new_forward=MistralPipelineForwards.mistral_model_forward,
+                policy=policy,
+            )
+
+        return policy
+
+    def get_held_layers(self) -> List[nn.Module]:
+        return super().get_held_layers()
+
 
 class MistralForCausalLMPolicy(MistralPolicy, ColossalMistralForCausalLMPolicy):
-    def preprocess(self):
-        # This is for forward compatibility to support newer version of transformers
-        self.model.model._use_flash_attention_2 = (
-            self.shard_config.enable_flash_attention
-        )
-        self.tie_weight = self.tie_weight_check()
-        return self.model
-
     @staticmethod
     def get_all_modules(config: PretrainedConfig) -> list[str]:
         modules = [
@@ -90,3 +319,60 @@ class MistralForCausalLMPolicy(MistralPolicy, ColossalMistralForCausalLMPolicy):
         super().pipeline_template_sanity_check(template)
         if "lm_head" not in template.modules_per_stage[-1]:
             raise ValueError("lm_head must be in the last stage.")
+
+    def module_policy(self) -> Dict[str | nn.Module, ModulePolicyDescription]:
+        from transformers.models.mistral.modeling_mistral import MistralForCausalLM
+
+        policy = super().module_policy()
+
+        if self.shard_config.enable_tensor_parallelism:
+            # add a new item for causal lm
+            target_module = VocabParallelLMHead1D
+            kwargs = {
+                "gather_output": not self.shard_config.parallel_output,
+                "make_vocab_size_divisible_by": self.shard_config.make_vocab_size_divisible_by,
+            }
+            methods_replacement = {
+                "forward": functools.partial(
+                    MistralForwards.mistral_for_causal_lm_forward_with_dist_cross_entropy,
+                    shard_config=self.shard_config,
+                )
+            }
+        else:
+            target_module = PaddingLMHead
+            kwargs = {
+                "make_vocab_size_divisible_by": self.shard_config.make_vocab_size_divisible_by
+            }
+            methods_replacement = None
+
+        policy.update(
+            {
+                MistralForCausalLM: ModulePolicyDescription(
+                    sub_module_replacement=[
+                        SubModuleReplacementDescription(
+                            suffix="lm_head",
+                            target_module=target_module,
+                            kwargs=kwargs,
+                        )
+                    ],
+                    method_replacement=methods_replacement,
+                )
+            }
+        )
+
+        if self.pipeline_stage_manager:
+            # set None as default
+            self.set_pipeline_forward(
+                model_cls=MistralForCausalLM,
+                new_forward=MistralPipelineForwards.mistral_for_causal_lm_forward,
+                policy=policy,
+            )
+
+        return policy
+
+    def get_held_layers(self) -> List[nn.Module]:
+        stage_manager = self.pipeline_stage_manager
+        held_layers = super().get_held_layers()
+        if stage_manager.is_last_stage(ignore_chunk=True):
+            held_layers.append(self.model.lm_head)
+        return held_layers
