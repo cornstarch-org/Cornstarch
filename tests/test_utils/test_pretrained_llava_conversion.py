c2bcb0b875e726769c68b3d37a9ce467cbf586f1
shiqihe <shiqihe@umich.edu>
2024-07-22 05:15:24 +0000

    Fix vision_feature_layer settings in llava

--- tests/test_utils/test_pretrained_llava_conversion.py
+++ tests/test_utils/test_pretrained_llava_conversion.py
@@ -50,7 +50,7 @@ def test_multimodal_model_generation(
     # create cornstarch llava model
     cornstarch_llava = MultimodalModel.from_pretrained_multimodal_model(
         model_id=pretrained_model_name_or_path,
-    ).to(torch.float16)
+    ).to(dtype=torch.float16, device="cuda")
 
     llava_model = LlavaForConditionalGeneration.from_pretrained(
         pretrained_model_name_or_path,
@@ -58,27 +58,44 @@ def test_multimodal_model_generation(
         torch_dtype="auto",
         device_map="cuda",
     )
+    llava_model.config.vision_feature_layer = -1
+    llava_model.config.vision_feature_select_strategy = "full"
 
     # ToDo: compare weights
-    prompt = "<image>USER: What are these? ASSISTANT:"
     image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 
     llava_processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path)
 
     raw_image = Image.open(requests.get(image_file, stream=True).raw)
+
+    prompt = "<image>USER: What are these? ASSISTANT:"
     llava_inputs = llava_processor(prompt, raw_image, return_tensors="pt").to(
-        0, llava_model.dtype
+        dtype=torch.float16, device="cuda"
     )
-
+    llava_inputs["input_ids"][0][0], llava_inputs["input_ids"][0][1] = llava_inputs["input_ids"][0][1].clone(), llava_inputs["input_ids"][0][0].clone()
+    
     llava_output = llava_model.generate(
-        **llava_inputs, max_new_tokens=20, do_sample=False, vision_feature_layer=-1
-    )
-
-    cornstarch_output = cornstarch_llava.generate(
         **llava_inputs,
+        max_new_tokens=20,
+        do_sample=False,
+        vision_feature_layer=-1,
+        pad_token_id=llava_processor.tokenizer.eos_token_id,
+    )
+    print(llava_processor.decode(llava_output[0][2:], skip_special_tokens=True))
+
+    prompt = "USER: What are these? ASSISTANT:"
+    cornstarch_inputs = llava_processor(prompt, raw_image, return_tensors="pt").to(
+        dtype=torch.float16, device="cuda"
+    )
+    cornstarch_output = cornstarch_llava.generate(
+        **cornstarch_inputs,
         max_new_tokens=20,
         do_sample=False,
         pad_token_id=llava_processor.tokenizer.eos_token_id,
     )
+    print(llava_processor.decode(cornstarch_output[0], skip_special_tokens=True))
 
-    print(llava_processor.decode(cornstarch_output[0][2:], skip_special_tokens=True))
+    # print("***", llava_model.config.vision_feature_layer, llava_model.config.vision_feature_select_strategy)
+    # print("!!!", cornstarch_output)
+
+    
