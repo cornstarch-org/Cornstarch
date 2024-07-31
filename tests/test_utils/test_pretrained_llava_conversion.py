090d2400f80fbcecb11c0871f43c212546304f42
shiqihe <shiqihe@umich.edu>
2024-07-31 01:00:20 +0000

    Bug fix in test file and remove unused args

--- tests/test_utils/test_pretrained_llava_conversion.py
+++ tests/test_utils/test_pretrained_llava_conversion.py
@@ -17,35 +17,20 @@ from cornstarch.models.multimodal_language_model import (
     MultimodalModel,
 )
 
-vision_model_name_or_path_list = [
-    ("openai/clip-vit-large-patch14-336", CLIPVisionConfig, CLIPVisionModel),
+
+pretrained_model_name_or_path_list = [
+    "llava-hf/llava-1.5-7b-hf"
 ]
 
-language_model_name_or_path_list = [
-    "lmsys/vicuna-7b-v1.5",
-]
-
-pretrained_model_name_or_path_list = ["llava-hf/llava-1.5-7b-hf"]
-
-
-@pytest.mark.parametrize(
-    "vision_model_name_or_path", vision_model_name_or_path_list, ids=lambda x: x[0]
-)
-@pytest.mark.parametrize("text_model_name_or_path", language_model_name_or_path_list)
 @pytest.mark.parametrize(
     "pretrained_model_name_or_path", pretrained_model_name_or_path_list
 )
 def test_multimodal_model_generation(
-    vision_model_name_or_path: tuple[
-        str, Type[PretrainedConfig], Type[PreTrainedModel]
-    ],
-    text_model_name_or_path: str,
     pretrained_model_name_or_path: str,
-    tmp_path: pathlib.Path,
 ):
     # create cornstarch llava model
     cornstarch_llava = MultimodalModel.from_pretrained_multimodal_model(
-        model_id=pretrained_model_name_or_path,
+        pretrained_model_id=pretrained_model_name_or_path,
     ).to(dtype=torch.float16, device="cuda")
 
     # create llava model
