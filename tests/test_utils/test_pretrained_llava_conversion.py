27d47ea5b533e7b5d8a639f3d2dbc98b5d218a4f
Insu Jang <insujang@umich.edu>
2024-08-01 20:06:20 +0000

    generalize function not to focus on llava rename llava to hf/cornstarch remove unused package import add comments

--- tests/test_utils/test_pretrained_llava_conversion.py
+++ tests/test_utils/test_pretrained_llava_conversion.py
@@ -1,4 +1,3 @@
-import pathlib
 from typing import Type
 
 import pytest
@@ -7,57 +6,53 @@ import torch
 from PIL import Image
 from transformers import (
     AutoProcessor,
-    LlavaForConditionalGeneration,
-    LlavaNextForConditionalGeneration,
-    PretrainedConfig,
     PreTrainedModel,
 )
-from transformers.models.clip import CLIPVisionConfig, CLIPVisionModel
+from transformers.models.llava import LlavaForConditionalGeneration
+from transformers.models.llava_next.modeling_llava_next import (
+    LlavaNextForConditionalGeneration,
+)
 
 from cornstarch.models.multimodal_language_model import (
     MultimodalModel,
 )
 
-pretrained_model_name_or_path_list = [
-    "llava-hf/llava-1.5-7b-hf",
-    # "llava-hf/llava-v1.6-vicuna-7b-hf"
+model_name_classes = [
+    ("llava-hf/llava-1.5-7b-hf", LlavaForConditionalGeneration),
+    ("llava-hf/llava-v1.6-vicuna-7b-hf", LlavaNextForConditionalGeneration),
 ]
 
 
 @pytest.mark.parametrize(
-    "pretrained_model_name_or_path", pretrained_model_name_or_path_list
+    "model_name_class", model_name_classes, ids=lambda x: x[0].split("/")[1]
 )
 def test_multimodal_model_generation(
-    pretrained_model_name_or_path: str,
+    model_name_class: tuple[str, Type[PreTrainedModel]],
 ):
-    # todo: currently does not support llava-v1.6
+    model_name, model_cls = model_name_class
+
+    # TODO: currently does not support llava-v1.6 due to its image patching
+    if "llava-v1.6" in model_name:
+        pytest.skip("llava-v1.6 not supported")
 
     # create cornstarch llava model
-    cornstarch_llava = MultimodalModel.from_pretrained_multimodal_model(
-        pretrained_model_id=pretrained_model_name_or_path,
-    ).to(dtype=torch.float16, device="cuda")
+    cornstarch_model: MultimodalModel = (
+        MultimodalModel.from_pretrained_multimodal_model(
+            pretrained_model_id=model_name,
+        ).to(dtype=torch.float16, device="cuda")
+    )
+    cornstarch_model.train(mode=False)
 
-    # create llava model
-    if "llava-1.5" in pretrained_model_name_or_path:
-        llava_model = LlavaForConditionalGeneration.from_pretrained(
-            pretrained_model_name_or_path,
-            revision="main",
-            torch_dtype="auto",
-            device_map="cuda",
-        )
-    elif "llava-v1.6" in pretrained_model_name_or_path:
-        llava_model = LlavaNextForConditionalGeneration.from_pretrained(
-            pretrained_model_name_or_path,
-            revision="main",
-            torch_dtype="auto",
-            device_map="cuda",
-        )
+    hf_model: PreTrainedModel = model_cls.from_pretrained(model_name).to(
+        dtype=torch.float16, device="cuda"
+    )
+    hf_model.train(mode=False)
 
     # config vision feature layer to match cornstarch settings
-    llava_model.config.vision_feature_layer = -1
-    llava_model.config.vision_feature_select_strategy = "full"
+    hf_model.config.vision_feature_layer = -1
+    hf_model.config.vision_feature_select_strategy = "full"
 
-    llava_processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path)
+    processor = AutoProcessor.from_pretrained(model_name)
 
     # loading sample image file
     image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
@@ -65,40 +60,47 @@ def test_multimodal_model_generation(
 
     # llava text generation
     prompt = "<image>USER: What are these? ASSISTANT:"
-    llava_inputs = llava_processor(prompt, raw_image, return_tensors="pt").to(
+    hf_inputs = processor(prompt, raw_image, return_tensors="pt").to(
         dtype=torch.float16, device="cuda"
     )
-    llava_inputs["input_ids"][0][0], llava_inputs["input_ids"][0][1] = (
-        llava_inputs["input_ids"][0][1].clone(),
-        llava_inputs["input_ids"][0][0].clone(),
+
+    # currently hf llava replaces <image> with actual image embeddings,
+    # thus bos_token is located before image embedings, while cornstarch
+    # simply prepends image embeddings to the input sequence,
+    # thus the location of bos_token is different.
+    # to match the result, we swap bos_token and <image>
+    # FIXME: it does not guarantee correctness. Fix it.
+    hf_inputs["input_ids"][0][0], hf_inputs["input_ids"][0][1] = (
+        hf_inputs["input_ids"][0][1].clone(),
+        hf_inputs["input_ids"][0][0].clone(),
     )
 
-    llava_output = llava_model.generate(
-        **llava_inputs,
+    hf_output = hf_model.generate(
+        **hf_inputs,
         max_new_tokens=20,
         do_sample=False,
         vision_feature_layer=-1,
-        pad_token_id=llava_processor.tokenizer.eos_token_id,
+        pad_token_id=processor.tokenizer.eos_token_id,
     )
-    llava_text_output = (
-        llava_processor.decode(llava_output[0][2:], skip_special_tokens=True)
+    hf_text_output = (
+        processor.decode(hf_output[0][2:], skip_special_tokens=True)
         .split("ASSISTANT:")[-1]
         .strip()
     )
 
     # cornstarch text generation
     prompt = "USER: What are these? ASSISTANT:"
-    cornstarch_inputs = llava_processor(prompt, raw_image, return_tensors="pt").to(
+    cornstarch_inputs = processor(prompt, raw_image, return_tensors="pt").to(
         dtype=torch.float16, device="cuda"
     )
-    cornstarch_output = cornstarch_llava.generate(
+    cornstarch_output = cornstarch_model.generate(
         **cornstarch_inputs,
         max_new_tokens=20,
         do_sample=False,
-        pad_token_id=llava_processor.tokenizer.eos_token_id,
+        pad_token_id=processor.tokenizer.eos_token_id,
     )
-    cornstarch_text_output = llava_processor.decode(
+    cornstarch_text_output = processor.decode(
         cornstarch_output[0], skip_special_tokens=True
     )
 
-    assert llava_text_output == cornstarch_text_output
+    assert hf_text_output == cornstarch_text_output
