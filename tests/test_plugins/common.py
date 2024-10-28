ee7b32ef4e3c4125f72c252a7afb52ae2c911986
Insu Jang <insujang@umich.edu>
2024-10-28 14:36:09 +0000

    add missing test template

--- tests/test_plugins/common.py
+++ tests/test_plugins/common.py
@@ -21,6 +21,9 @@ encoder1_template = PipelineTemplate(
 encoder2_template = PipelineTemplate(
     "encoder2", [["layer.0", "layer.1"], ["layer.2", "layer.3"], ["layer.4", "layer.5"]]
 )
+encoder3_template = PipelineTemplate(
+    "encoder3", [["layer.0", "layer.1", "layer.2"], ["layer.3", "layer.4"]]
+)
 
 llm_template_2stages = PipelineTemplate(
     "llm", [["layer.0", "layer.1", "layer.2"], ["layer.3", "layer.4"]]
