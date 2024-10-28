precision anymask fwd fixed, bwd not ok still

cornstarch/kernel/interface.py
@@ -83,20 +83,24 @@ def _flash_attn_anymask_backward(
     rng_state: Optional[torch.Tensor] = None,
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     assert dout.is_contiguous()
-    # assert q.stride() == k.stride() == v.stride() == out.stride() == dout.stride()
+    assert q.stride() == k.stride() == v.stride() == out.stride() == dout.stride()
     # TODO(@runyu) stride is not good, check this, maybe the bug
     assert q.shape == k.shape == v.shape, f"Shape mismatch: q: {q.shape}, k: {k.shape}, v: {v.shape}"
     assert q.shape == out.shape == dout.shape, f"Shape mismatch: q: {q.shape}, out: {out.shape}, dout: {dout.shape}"
     HEAD_DIM = q.shape[-1]
-    dq = torch.empty_like(q)
-    dk = torch.empty_like(k)
-    dv = torch.empty_like(v)
+    if dq is None:
+        dq = torch.empty_like(q)
+    if dk is None:
+        dk = torch.empty_like(k)
+    if dv is None:
+        dv = torch.empty_like(v)
     BATCH, N_HEAD, N_CTX = q.shape[:3]
     PRE_BLOCK = 128
     NUM_WARPS, NUM_STAGES = 4, 5
     BLOCK_M1, BLOCK_N1, BLOCK_M2, BLOCK_N2 = 32, 128, 128, 32
     BLK_SLICE_FACTOR = 2
     RCP_LN2 = 1.4426950408889634  # = 1.0 / ln(2)
+    softmax_lse = softmax_lse * RCP_LN2
     arg_k = k
     arg_k = arg_k * (softmax_scale * RCP_LN2)
     PRE_BLOCK = 128

cornstarch/shardformer/layers/ring_attn.py
@@ -177,24 +177,23 @@ def ring_flash_attn_anymask_forward(
             next_v: torch.Tensor = comm.send_recv(v)
             comm.commit()
 
-        if not causal or step <= comm.rank:
-            params = get_default_args(ATTN_IMPL).copy()
-            params.update(
-                {
-                    "q": q,
-                    "k": k,
-                    "v": v,
-                    "mask": mask,
-                    "dropout_p": dropout_p,
-                    "softmax_scale": softmax_scale,
-                    "window_size_left": window_size_left,
-                    "window_size_right": window_size_right,
-                    "alibi_slopes": alibi_slopes,
-                    "return_softmax": True and dropout_p > 0,
-                }
-            )
-            block_out, block_lse, _, _ = ATTN_IMPL(**params)
-            out, lse = update_out_and_lse(out, lse, block_out, block_lse)
+        params = get_default_args(ATTN_IMPL).copy()
+        params.update(
+            {
+                "q": q,
+                "k": k,
+                "v": v,
+                "mask": mask,
+                "dropout_p": dropout_p,
+                "softmax_scale": softmax_scale,
+                "window_size_left": window_size_left,
+                "window_size_right": window_size_right,
+                "alibi_slopes": alibi_slopes,
+                "return_softmax": True and dropout_p > 0,
+            }
+        )
+        block_out, block_lse, _, _ = ATTN_IMPL(**params)
+        out, lse = update_out_and_lse(out, lse, block_out, block_lse)
 
         if step + 1 != comm.world_size:
             comm.wait()
@@ -202,7 +201,7 @@ def ring_flash_attn_anymask_forward(
             v = next_v
 
     out = out.to(q.dtype)
-    lse = lse.squeeze(dim=-1).transpose(1, 2)
+    lse = lse.squeeze(dim=-1)
     return out, lse
 
 
@@ -241,43 +240,77 @@ def ring_flash_attn_anymask_backward(
             next_k = kv_comm.send_recv(k)
             next_v = kv_comm.send_recv(v)
             kv_comm.commit()
-        if step <= kv_comm.rank:
-            params = get_default_args(ATTN_IMPL).copy()
-            params.update(
-                {
-                    "dout": dout,
-                    "q": q,
-                    "k": k,
-                    "v": v,
-                    "out": out,
-                    "softmax_lse": softmax_lse,
-                    "dq": block_dq_buffer,
-                    "dk": block_dk_buffer,
-                    "dv": block_dv_buffer,
-                    "mask": mask,
-                    "dropout_p": dropout_p,
-                    "softmax_scale": softmax_scale,
-                    "window_size_left": window_size_left,
-                    "window_size_right": window_size_right,
-                    "alibi_slopes": alibi_slopes,
-                    "deterministic": deterministic,
-                }
-            )
-            ATTN_IMPL(**params)
 
-            if dq is None:
-                dq = block_dq_buffer.to(torch.float32)
-                dk = block_dk_buffer.to(torch.float32)
-                dv = block_dv_buffer.to(torch.float32)
-            else:
-                dq += block_dq_buffer
-                d_kv_comm.wait()
-                dk = block_dk_buffer + next_dk
-                dv = block_dv_buffer + next_dv
-        elif step != 0:
+        params = get_default_args(ATTN_IMPL).copy()
+        params.update(
+            {
+                "dout": dout,
+                "q": q,
+                "k": k,
+                "v": v,
+                "out": out,
+                "softmax_lse": softmax_lse,
+                "dq": block_dq_buffer,
+                "dk": block_dk_buffer,
+                "dv": block_dv_buffer,
+                "mask": mask,
+                "dropout_p": dropout_p,
+                "softmax_scale": softmax_scale,
+                "window_size_left": window_size_left,
+                "window_size_right": window_size_right,
+                "alibi_slopes": alibi_slopes,
+                "deterministic": deterministic,
+            }
+        )
+        ATTN_IMPL(**params)
+
+        if dq is None:
+            dq = block_dq_buffer.to(torch.float32)
+            dk = block_dk_buffer.to(torch.float32)
+            dv = block_dv_buffer.to(torch.float32)
+        else:
+            dq += block_dq_buffer
             d_kv_comm.wait()
-            dk = next_dk
-            dv = next_dv
+            dk = block_dk_buffer + next_dk
+            dv = block_dv_buffer + next_dv
+
+        # if step <= kv_comm.world_size:
+        #     params = get_default_args(ATTN_IMPL).copy()
+        #     params.update(
+        #         {
+        #             "dout": dout,
+        #             "q": q,
+        #             "k": k,
+        #             "v": v,
+        #             "out": out,
+        #             "softmax_lse": softmax_lse,
+        #             "dq": block_dq_buffer,
+        #             "dk": block_dk_buffer,
+        #             "dv": block_dv_buffer,
+        #             "mask": mask,
+        #             "dropout_p": dropout_p,
+        #             "softmax_scale": softmax_scale,
+        #             "window_size_left": window_size_left,
+        #             "window_size_right": window_size_right,
+        #             "alibi_slopes": alibi_slopes,
+        #             "deterministic": deterministic,
+        #         }
+        #     )
+        #     ATTN_IMPL(**params)
+
+        #     if dq is None:
+        #         dq = block_dq_buffer.to(torch.float32)
+        #         dk = block_dk_buffer.to(torch.float32)
+        #         dv = block_dv_buffer.to(torch.float32)
+        #     else:
+        #         dq += block_dq_buffer
+        #         d_kv_comm.wait()
+        #         dk = block_dk_buffer + next_dk
+        #         dv = block_dv_buffer + next_dv
+        # elif step != 0:
+        #     d_kv_comm.wait()
+        #     dk = next_dk
+        #     dv = next_dv
 
         if step + 1 != kv_comm.world_size:
             kv_comm.wait()

tests/tmp_test/test_kernel/test_attn.py
@@ -63,8 +63,7 @@ def test_op_any_mask(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
     M = mask = torch.randint(0, 2, (N_CTX, N_CTX), dtype=torch.uint8, device="cuda", requires_grad=False)
     mask = torch.broadcast_to(mask, (Z, H, N_CTX, N_CTX))
     p[:, :, M == 0] = float("-inf")
-    _, ref_softmax_lse = softmax_base2_to_e_with_scale(p, scale=sm_scale)
-    p = torch.softmax(p.float(), dim=-1).half()
+    p = torch.softmax(p.float(), dim=-1).to(dtype)
     ref_out = torch.matmul(p, v)
     ref_out.backward(dout)
     ref_dv, v.grad = v.grad.clone(), None
@@ -91,3 +90,4 @@ if __name__ == "__main__":
     pytest.main([__file__])
     # test_op_casual(1, 2, 1024, 64, causal=True, dtype=torch.bfloat16)
     # test_op_casual(1, 2, 1024, 64, causal=True, dtype=torch.float16)
+    # test_op_any_mask(1, 2, 128, 64, dtype=torch.bfloat16)