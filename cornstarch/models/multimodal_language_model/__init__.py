d59f0a48b0df41a7f84ae506199e79cf68afd51a
Insu Jang <insujang@umich.edu>
2025-01-26 22:23:55 +0000

    nits

--- cornstarch/models/multimodal_language_model/__init__.py
+++ cornstarch/models/multimodal_language_model/__init__.py
@@ -8,7 +8,7 @@ from .modeling_multimodal_language_model import (
     MultimodalModel,
     MultimodalProjector,
 )
-from .processing_multimodal_language_model import MultimodalModelProcessor
+from .processing_multimodal_language_model import MultimodalProcessor
 
 AutoConfig.register("multimodal-projector", MultimodalProjectorConfig)
 AutoModel.register(MultimodalProjectorConfig, MultimodalProjector)
