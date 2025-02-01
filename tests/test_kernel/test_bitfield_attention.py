03c704148085a1664bb8ed0b327b8aabb492489d
Insu Jang <insujang@umich.edu>
2025-02-01 18:12:42 +0000

    add triton attention implementation

---
+++ tests/test_kernel/test_bitfield_attention.py
@@ -0,0 +1,168 @@
+import math
+
+import pytest
+import torch
+from einops import rearrange, repeat
+
+from cornstarch.kernel.attention import flash_attn_func
+
+
+def construct_local_mask(
+    seqlen_q: int,
+    seqlen_k: int,
+    window_size: tuple[int, int] = (-1, -1),  # -1 means infinite window size
+    query_padding_mask=None,
+    key_padding_mask=None,
+    device=None,
+    key_leftpad=None,
+):
+    row_idx = rearrange(
+        torch.arange(seqlen_q, device=device, dtype=torch.long), "s -> s 1"
+    )
+    col_idx = torch.arange(seqlen_k, device=device, dtype=torch.long)
+    if key_leftpad is not None:
+        key_leftpad = rearrange(key_leftpad, "b -> b 1 1 1")
+        col_idx = repeat(col_idx, "s -> b 1 1 s", b=key_leftpad.shape[0])
+        col_idx = torch.where(col_idx >= key_leftpad, col_idx - key_leftpad, 2**32)
+    sk = (
+        seqlen_k
+        if key_padding_mask is None
+        else rearrange(key_padding_mask.sum(-1), "b -> b 1 1 1")
+    )
+    sq = (
+        seqlen_q
+        if query_padding_mask is None
+        else rearrange(query_padding_mask.sum(-1), "b -> b 1 1 1")
+    )
+    if window_size[0] < 0:
+        return col_idx > row_idx + sk - sq + window_size[1]
+    else:
+        sk = torch.full_like(col_idx, seqlen_k) if key_padding_mask is None else sk
+        return torch.logical_or(
+            col_idx > torch.minimum(row_idx + sk - sq + window_size[1], sk),
+            col_idx < row_idx + sk - sq - window_size[0],
+        )
+
+
+def reference_attention(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    attention_mask: torch.Tensor = None,
+) -> torch.Tensor:
+    """
+    Arguments:
+        q: (batch_size, seqlen_q, nheads, head_dim)
+        k: (batch_size, seqlen_k, nheads_k, head_dim)
+        v: (batch_size, seqlen_k, nheads_k, head_dim)
+        attention_mask: (seqlen_q, seqlen_k)
+
+    Return:
+        output: (batch_size, seqlen_q, nheads, head_dim)
+    """
+    q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+    if q.shape[1] % k.shape[1] != 0:
+        raise ValueError(
+            "Number of heads in q must be divisible by number of heads in k and v"
+        )
+    num_key_value_groups = q.shape[1] // k.shape[1]
+
+    if num_key_value_groups > 1:
+        from transformers.models.llama.modeling_llama import repeat_kv
+
+        k = repeat_kv(k, num_key_value_groups)
+        v = repeat_kv(v, num_key_value_groups)
+
+    head_dim = q.shape[-1]
+    attn_weights = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(head_dim)
+
+    if attention_mask is not None:
+        # attn_weights should be (batch_size, nheads, seqlen_q, seqlen_k)
+        # mask out with -inf for every (batch_size, nheads)
+        # using pattern of "q k -> 1 1 q k" and add it to attn_weights
+        # Expand to match the dimensions of attn_weights and attention_mask
+        attn_weights = attn_weights + torch.where(
+            attention_mask.expand(q.shape[0], q.shape[1], -1, -1) == 0,
+            float("-inf"),
+            0.0,
+        )
+
+    # upcast attention to fp32
+    attn_weights = torch.nn.functional.softmax(
+        attn_weights, dim=-1, dtype=torch.float32
+    ).to(q.dtype)
+    attn_weights = torch.nn.functional.dropout(attn_weights, p=0.0, training=True)
+    attn_output = torch.matmul(attn_weights, v)
+
+    assert attn_output.shape == q.shape
+
+    return attn_output.transpose(1, 2).contiguous()
+
+
+def get_mask(seqlen_q: int, seqlen_k: int, device: torch.device) -> torch.Tensor:
+    return torch.randint(0, 2, (seqlen_q, seqlen_k), device=device)
+
+
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16], ids=["fp16", "bf16"])
+@pytest.mark.parametrize("head_dim", [32, 64, 128])
+@pytest.mark.parametrize("seqlen", [(256, 256), (1024, 256), (256, 1024), (1024, 1024)])
+def test_attention(dtype: torch.dtype, head_dim: int, seqlen: tuple[int, int]):
+    device = torch.device("cuda")
+    batch_size = 2
+    num_heads = 12
+
+    seqlen_q, seqlen_k = seqlen
+
+    torch.random.manual_seed(0)
+
+    q = (
+        torch.randn(
+            batch_size,
+            seqlen_q,
+            num_heads,
+            head_dim,
+            device=device,
+            dtype=dtype,
+        )
+        .normal_(mean=0, std=0.5)
+        .requires_grad_(True)
+    )
+    k = (
+        torch.randn(
+            batch_size,
+            seqlen_k,
+            num_heads,
+            head_dim,
+            device=device,
+            dtype=dtype,
+        )
+        .normal_(mean=0, std=0.5)
+        .requires_grad_(True)
+    )
+    v = (
+        torch.randn(
+            batch_size,
+            seqlen_k,
+            num_heads,
+            head_dim,
+            device=device,
+            dtype=dtype,
+        )
+        .normal_(mean=0, std=0.5)
+        .requires_grad_(True)
+    )
+
+    mask = get_mask(seqlen_q, seqlen_k, device)
+
+    reference_out = reference_attention(q, k, v, mask)
+    triton_out = flash_attn_func(q, k, v, None, False, mask)
+
+    g = torch.randn_like(reference_out)
+    dq_ref, dk_ref, dv_ref = torch.autograd.grad(reference_out, (q, k, v), g)
+    dq, dk, dv = torch.autograd.grad(triton_out, (q, k, v), g)
+
+    torch.testing.assert_close(reference_out, triton_out, rtol=5e-3, atol=5e-3)
+    torch.testing.assert_close(dq_ref, dq, rtol=5e-3, atol=5e-3)
+    torch.testing.assert_close(dk_ref, dk, rtol=5e-3, atol=5e-3)
+    torch.testing.assert_close(dv_ref, dv, rtol=5e-3, atol=5e-3)
